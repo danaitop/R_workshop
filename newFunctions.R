806d618d3ca5feddc520dfe894c7ab073966e5e9
Danai Topouza <13dgt@queensu.ca>
2017-04-25 11:54:38 -0400

    new script

---
+++ newFunctions.R
@@ -0,0 +1,18 @@
+#How to make a new function
+
+adder <- function(num1, num2) {
+  return(num1 + num2)
+}
+
+adder(1,2)
+
+#fahrenheit to kelvin
+f2k <- function(fnum) {
+  return((fnum - 32) * 5 / 9 + 273.15)
+}
+
+#kelvin to fahrenheit
+
+k2f <- function(knum) {
+  return((knum - 273.15) * 9 / 5 + 32)
+}
